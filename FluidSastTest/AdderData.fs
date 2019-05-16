ee6deadf06b29844a7d27123c705dd9d3f3f3eb8
Rumyana Neykova (Staff) <csstrrn@brunel.ac.uk>
2019-05-16 19:54:30 +0100

    Add a test project

---
+++ FluidSastTest/AdderData.fs
@@ -0,0 +1,10 @@
+﻿namespace FluidSastTest
+
+module AdderData = 
+
+    [<Literal>]
+    let delims = """ [{"label" : "HELLO", "delims": {"delim1": [":"] , "delim2": [","] , "delim3": [";"] } }]"""
+
+
+    [<Literal>]
+    let typeAliasing = """ [] """
