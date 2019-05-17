1e3097c48458766d7546d5a27987944f846dcbd2
Rumyana Neykova (Staff) <csstrrn@brunel.ac.uk>
2019-05-17 19:25:36 +0100

    Test with Erased TP (working)

--- FluidSastTest/Adder.fs
+++ FluidSastTest/Adder.fs
@@ -13,40 +13,14 @@ module Adder =
         else -x
     
     type AdderC = 
-       Provided.TypeProviderFile<"FSM/FsmAsstC.txt", "Adder", "C"
-           ,"Config/configC.yaml", Delimiter=AdderData.delims
+       Provided.STP<"FSM\FSMAsstC.txt", "Adder", "C"
+           ,"Config\configC.yaml", Delimiter=AdderData.delims
            ,TypeAliasing=AdderData.typeAliasing, AssertionsOn=true, ScribbleSource= ScribbleSource.File>
     
+    let hello (x:int) =  
+        printf "Received %i" x
     
     let S = AdderC.S.instance
     let client = new AdderC()
-    let c = client.Init().receiveHELLO(S).finish()
-
-    (*
-    let numIter = 1000
-
-    let rec adderRec a b iter (c0:AdderC.State12) = 
-       let res = new DomainModel.Buf<int>()
-       //let c1= c0.receiveHELLO(S, res)
-       let c = c0.sendHELLO(S, "hello")
-       match iter with
-           |0 -> 
-               let c1 = c.sendBYE(S)
-               let c2 = c1.receiveBYE(S)
-               printfn "Fibo : %d" b
-               let finalc = c2.finish()
-               finalc
-           |n -> 
-               let c1 = c.sendADD(S, a)
-               printfn "Send ADD"   
-               let c2 = c1.receiveRES(S, res)
-               (*let foo s = if s > 0 then true else false 
-               let result = foo (res.getValue())*)
-               adderRec b (res.getValue()) (n-1) c2
-
-    let client = new AdderC()
-    let res = new DomainModel.Buf<int>()
-    let c = client.Init().receiveHELLO(S, res).sendHELLO(S, 2)
-    let s1 = c |> adderRec 1 1 3
-    
-    *)
+    let c = client.Init()//receiveHELLO(S).finish()
+    let s1 = c.receiveHELLO(S, hello).finish()
--- FluidSastTest/AdderData.fs
+++ FluidSastTest/AdderData.fs
@@ -3,8 +3,14 @@
 module AdderData = 
 
     [<Literal>]
-    let delims = """ [{"label" : "HELLO", "delims": {"delim1": [":"] , "delim2": [","] , "delim3": [";"] } }]"""
+    let delims = """ [ {"label" : "SUM", "delims": {"delim1": [":"] , "delim2": [","] , "delim3": [";"] } },
+                   {"label" : "RES", "delims": {"delim1": [":"] , "delim2": [","] , "delim3": [";"] } }, 
+                   {"label" : "ADD", "delims": {"delim1": [":"] , "delim2": [","] , "delim3": [";"] } }, 
+                   {"label" : "BYE", "delims": {"delim1": [":"] , "delim2": [","] , "delim3": [";"] } }, 
+                   {"label" : "close", "delims": {"delim1": [":"] , "delim2": [","] , "delim3": [";"] } }, 
+                   {"label" : "HELLO", "delims": {"delim1": [":"] , "delim2": [","] , "delim3": [";"] } }]"""
 
 
     [<Literal>]
-    let typeAliasing = """ [] """
+    let typeAliasing = """ [ {"alias" : "int", "type": "System.Int32"}, 
+                {"alias" : "string", "type": "System.String"}] """
